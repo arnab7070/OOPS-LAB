e7a0eed5f28bbdd098464d12caecc134a47b100d
Arnab Nandi <arnabnandi7074@gmail.com>
2023-11-16 19:13:37 +0530

    Basic Setup

---
+++ Assignment 8/Question1.java
@@ -0,0 +1,5 @@
+public class Question1 {
+    public static void main(String[] args) {
+
+    }
+}
--- main.java
+++
@@ -1,9 +0,0 @@
-/**
- * main
- */
-public class main {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
