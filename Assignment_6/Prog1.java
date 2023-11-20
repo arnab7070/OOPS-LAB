cc8618c85aa6bdcc72c5233276e3d11ec74046b6
Arnab Nandi <arnabnandi7074@gmail.com>
2023-11-20 15:57:57 +0530

    Updated till Package

--- Assignment_6/Prog1.java
+++ Assignment_6/Prog1.java
@@ -1,20 +1,28 @@
 // Show that ordinary block is executed when object is created and also the order of execution of these blocks (for multiple blocks/
 // inherited block).
-class A{
+class A {
     {
         System.out.println("1st instance block of parent(A)");
     }
     {
         System.out.println("2nd instance block of parent(A)");
     }
+    static {
+        System.out.println("Static block of Child (A)");
+    }
     {
         System.out.println("3rd instance block of parent(A)");
     }
-    A(){
-         System.out.println("Constuctor-->A");
+
+    A() {
+        System.out.println("Constuctor-->A");
     }
 }
-class B extends A{
+
+class B extends A {
+    static {
+        System.out.println("Static block of Child (B)");
+    }
 
     {
         System.out.println("1st instance block of child(B)");
@@ -23,14 +31,16 @@ class B extends A{
         System.out.println("2nd instance block of child(B)");
     }
     {
-         System.out.println("3rd instance block of child(B)");
+        System.out.println("3rd instance block of child(B)");
     }
-    B(){
-         System.out.println("Constuctor-->B");
+
+    B() {
+        System.out.println("Constuctor-->B");
     }
 }
-class Prog1{
-    public static void main(String[]args){
+
+class Prog1 {
+    public static void main(String[] args) {
         B b = new B();
 
     }
