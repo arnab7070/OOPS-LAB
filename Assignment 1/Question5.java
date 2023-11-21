255a6a705d1dfb88e6ef64625c13448905a126da
BIBHAB22 <bibhabindia@gmail.com>
2023-11-21 14:57:54 +0530

    update in Question 5

--- Assignment 1/Question5.java
+++ Assignment 1/Question5.java
@@ -1,24 +1,12 @@
-public class Question5 {
+class Question5 {
     public static void main(String[] args) {
-        String currentSequence = "AMM";
-        int numberOfTerms = 5;
-
-        System.out.println("Alphabet Sequence:");
-        for (int i = 0; i < numberOfTerms; i++) {
-            System.out.println(currentSequence);
-            currentSequence = generateNextTerm(currentSequence);
+        char startLetter = 'A';
+        for (int i = 0; i < 5; i++) {
+            System.out.print(startLetter);
+            System.out.print((char)(startLetter + 12));
+            System.out.print((char)(startLetter + 12));
+            System.out.print(" ");
+            startLetter = (char)(startLetter + 2);
         }
-    }
-    private static String generateNextTerm(String currentTerm) {
-        StringBuilder nextTerm = new StringBuilder();
-        for (char c : currentTerm.toCharArray()) {
-            if (Character.isLetter(c)) {
-                char nextLetter = (char) ((c - 'A' + 1) % 26 + 'A');
-                nextTerm.append(nextLetter);
-            } else {
-                nextTerm.append(c);
-            }
-        }
-        return nextTerm.toString();
     }
 }
